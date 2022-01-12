e058ca29c45a442e8906aa0f1b1b1155b2b17523
MORIAdriel <adrielmori@outlook.com>
2022-01-12 18:27:01 -0300

    Pagination Layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,10 @@
+/* Limport Pagination from "components/Pagination";sta da página home que mostra todos os filmes no bando de dados */
+import Pagination from "components/Pagination";
+import './styles.css';
+
 function Listing(){
     return(
-        <h1>Páigna de Listagem</h1>
+        <Pagination />/* Aplicando as setas para que a paginação */
     )
 }
 
