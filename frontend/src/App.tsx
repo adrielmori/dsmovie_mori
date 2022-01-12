a7dafd14d0bc6b6268a719aa741e441eba89d89c
MORIAdriel <adrielmori@outlook.com>
2022-01-12 14:51:49 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import Navbar from './components/Navbar';
-/* Componente React navBar: função do JS  */
+/* Componentes para confugurar as rotas */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "components/Navbar"; /* Componente React navBar: função do JS  */
+import Listing from 'pages/Listing'; /* Página de listagem de filmes */
+import Form from 'pages/Form'; /* Página de formulário */
 
 function App() {
-  /* navebar para o HTML */ 
+  /* navebar para o HTML */
 
   return (
-    <Navbar/>
+    <BrowserRouter> {/* Ininciar configuração da rota para padronizar o Navbar da interface */}
+      <Navbar /> {/* A parte de cima da aplicação */}
+      <Routes> {/* Configura cada página individualmente */}
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} /> {/* Configura para cada Id de cada Filme */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return(
+        <h1>Páigna de Listagem</h1>
+    )
+}
+
+export default Listing;
