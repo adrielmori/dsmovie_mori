3574ede6b2fb558aeae36f110819f2d979992281
MORIAdriel <adrielmori@outlook.com>
2022-01-26 20:23:34 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,12 +4,38 @@ import Pagination from "components/Pagination";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
 import "./styles.css";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
-  //Para fazer uma requisição
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
-    console.log(response.data);
+  //mantendo o estado no componente
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
   });
+
+  //executa somente quando é solicitado
+  useEffect(() => {
+    //Para fazer uma requisição
+    //Tem que passar a difinição Base Url definido no requisitos
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id=title`)//sort=id: garante que a busca do filme será por Id, na mesma ordem
+      .then(response => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
+
+  //Redenrizando com os objetos do
 
   return (
     <>
@@ -30,21 +56,12 @@ function Listing() {
                     Extra large    xl    ≥1200px
                     Extra extra large  xxl	≥1400px*/}
 
-          <div className="col-sm-6 col-log-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-log-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-log-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-log-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-log-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-log-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
+          {/* Analisa filmes de forma individual */}
         </div>
       </div>
     </>
